94e0ef520e582302a23b1237ec60e154ce8e0a75
tim.hofmann <tim.hofmann@students.fhnw.ch>
2023-10-17 14:45:24 +0200

    init Spring Boot

---
+++ src/main/java/ch/fhnw/therewrite/TheRewriteApplication.java
@@ -0,0 +1,13 @@
+package ch.fhnw.therewrite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheRewriteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TheRewriteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ch/fhnw/therewrite/TheRewriteApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.fhnw.therewrite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheRewriteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
