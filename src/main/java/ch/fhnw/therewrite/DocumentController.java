8da0c3445974b2c1b13ccb1b9c2b0bdfb6e5e854
tim.hofmann <tim.hofmann@students.fhnw.ch>
2023-10-26 16:44:33 +0200

    api rest example to return a pdf sample.pdf added into new uploads folder pdf source is easily modifiable with ?file get paramter (index.html changes)

--- src/main/java/ch/fhnw/therewrite/DocumentController.java
+++ src/main/java/ch/fhnw/therewrite/DocumentController.java
@@ -1,21 +1,32 @@
 package ch.fhnw.therewrite;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
 @RestController
 public class DocumentController {
-    @GetMapping("/PDF")
+    @GetMapping("/pdf")
     @ResponseBody
     public String pdf() throws IOException {
         Resource resource = new ClassPathResource("static/index.html");
         Path path = resource.getFile().toPath();
         return new String(Files.readAllBytes(path));
+    }
+    @GetMapping(
+            value = "/pdf-download",
+            produces = MediaType.APPLICATION_PDF_VALUE
+    )
+    public @ResponseBody byte[] getImage() throws IOException {
+        Resource resource = new ClassPathResource("uploads/sample.pdf");
+        InputStream in = resource.getInputStream();
+        return in.readAllBytes();
     }
 }
