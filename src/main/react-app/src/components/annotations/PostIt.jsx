c9d223228de4cff8442fd0f44a90fa262f585193
Shenia Scherer <shenia.scherer@students.fhnw.ch>
2023-11-17 22:54:48 +0100

    bare bones postit

--- src/main/react-app/src/components/annotations/PostIt.jsx
+++ src/main/react-app/src/components/annotations/PostIt.jsx
@@ -1,13 +1,14 @@
 import '../../style/basic.css';
 import '../../style/list.css';
+import {useState} from "react";
 
-function PostIt() {
-
+export default function PostIt({ color }) {
+    const [postitText,setPostitText] = useState("");
+    
     return (
-        <div>
-
+        <div className="post-it">
+            <img src='../../../../resources/assets/postits/post-it-'{color}'.svg' className="post-it-svg" alt="post-its"/>
+            <textarea className="post-it-input" value={postitText} onChange={e => setPostitText(e.target.value)}></textarea>
         </div>
     );
 }
-
-export default PostIt;
