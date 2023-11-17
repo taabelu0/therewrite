9851e32883f650ad758b9268e6303ccc70221a44
Shenia Scherer <shenia.scherer@students.fhnw.ch>
2023-11-18 00:16:30 +0100

    component post-it done

--- src/main/react-app/src/components/annotations/PostIt.jsx
+++ src/main/react-app/src/components/annotations/PostIt.jsx
@@ -1,14 +1,32 @@
-import '../../style/basic.css';
-import '../../style/list.css';
-import {useState} from "react";
+import React, {useState} from 'react';
+import '../../style/post-it.css';
+import GreenPostIt from "./postits/post-it-green.png";
+import RedPostIt from "./postits/post-it-red.png";
+import YellowPostIt from "./postits/post-it-yellow.png";
 
-export default function PostIt({ color }) {
-    const [postitText,setPostitText] = useState("");
-    
+export default function PostIt({color,top,left,text}) {
+    const [postitText, setPostitText] = useState(text);
+    const postitImages = {
+        green: GreenPostIt,
+        red: RedPostIt,
+        yellow: YellowPostIt
+    };
+
+    const postit = postitImages[color];
+
+    if (postit === null) {
+        return <p>No post-it available.</p>;
+    }
+
     return (
-        <div className="post-it">
-            <img src='../../../../resources/assets/postits/post-it-'{color}'.svg' className="post-it-svg" alt="post-its"/>
-            <textarea className="post-it-input" value={postitText} onChange={e => setPostitText(e.target.value)}></textarea>
+        <div className="post-it"
+             style={{top:top + "px" || 0,left:left + "px" || 0}}>
+            <img src={postit} alt={`${color} post-it`} className="post-it-png"/>
+            <textarea
+                className="post-it-input"
+                value={postitText}
+                onChange={event => setPostitText(event.target.value)}
+            />
         </div>
     );
 }
