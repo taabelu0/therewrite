cf7fabde970dc74a17b58222060ffeed6a64f773
tim.hofmann <tim.hofmann@students.fhnw.ch>
2023-11-28 08:41:34 +0100

    frontend build.sh uses relative urls now

--- src/main/react-app/build.sh
+++ src/main/react-app/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 rm -r build/*
-echo "REACT_APP_BASE_URL=https://rewrite-your.work" > .env
-echo "REACT_APP_API_URL=https://rewrite-your.work" >> .env
+echo "REACT_APP_BASE_URL=" > .env
+echo "REACT_APP_API_URL=" >> .env
 echo "NODE_ENV=production" >> .env
 npm run build
 rm -r ../resources/static/*
