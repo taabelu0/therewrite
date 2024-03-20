e2befbc6a85b3b008768f24ad7ce58c589b5c110
Luca Bertonazzi <luca.bertonazzi@students.fhnw.ch>
2024-03-20 15:56:53 +0100

    Revert "Merge branch 'annotation-owner' into 'develop'"
    This reverts merge request !44

--- src/main/react-app/src/apis/config/axiosConfig.js
+++ src/main/react-app/src/apis/config/axiosConfig.js
@@ -2,7 +2,7 @@ import axios from "axios"
 
 export const baseURL = process.env.REACT_APP_API_URL;
 
-export const api = axios.create({baseURL, withCredentials: true})
+export const api = axios.create({baseURL})
 
 // defining a custom error handler for all APIs
 const errorHandler = (error) => {
