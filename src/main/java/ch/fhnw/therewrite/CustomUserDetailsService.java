9c09639d08d4dd94f6594ee88fc17329f9c1ce68
tim.hofmann <tim.hofmann@students.fhnw.ch>
2024-05-22 16:44:33 +0200

    setup for admin user

--- src/main/java/ch/fhnw/therewrite/CustomUserDetailsService.java
+++ src/main/java/ch/fhnw/therewrite/CustomUserDetailsService.java
@@ -1,11 +1,17 @@
 package ch.fhnw.therewrite;
 
+import ch.fhnw.therewrite.data.Role;
 import ch.fhnw.therewrite.data.User;
 import ch.fhnw.therewrite.repository.UserRepository;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import java.util.Collection;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Service
 public class CustomUserDetailsService implements UserDetailsService {
@@ -25,7 +31,13 @@ public class CustomUserDetailsService implements UserDetailsService {
         return org.springframework.security.core.userdetails.User.builder()
                 .username(user.getUsername())
                 .password(user.getPassword())
-                .roles("USER")
+                .authorities(getAuthorities(user.getRoles()))
                 .build();
+    }
+
+    private Collection<? extends GrantedAuthority> getAuthorities(Set<Role> roles) {
+        return roles.stream()
+                .map(role -> new SimpleGrantedAuthority(role.getName()))
+                .collect(Collectors.toList());
     }
 }
---
+++ src/main/java/ch/fhnw/therewrite/data/Role.java
@@ -0,0 +1,38 @@
+package ch.fhnw.therewrite.data;
+
+import jakarta.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String name;
+
+    @ManyToMany(mappedBy = "roles")
+    private Set<User> users = new HashSet<>();
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Set<User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(Set<User> users) {
+        this.users = users;
+    }
+}
