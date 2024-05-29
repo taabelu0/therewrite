0f37f0154b28c81653ac65e1b990540636ddaaad
DamjanStojanovic <damjan.stojanovic@students.fhnw.ch>
2024-05-29 13:45:06 +0200

    Fixed error with changing category, no need to click on pen icon anymore.

--- src/main/react-app/src/components/annotations/SidebarAnnotation.jsx
+++ src/main/react-app/src/components/annotations/SidebarAnnotation.jsx
@@ -182,7 +182,7 @@ function SidebarAnnotation({
             <div onClick={(event) => onSelection(event, annotation.id)} id={'sidebar-' + annotation.id}
                  className={`sidebar-annotation sidebar-annotation-${annotation.category.toLowerCase()}`}>
                 <div className="sidebar-annotation-header">
-                    <div className="sidebar-option-change" onClick={switchShowCategories}></div>
+                    <div className="sidebar-option-change"></div>
                     {showCategories && (
                         <div className="sidebar-annotation-optionmenu-cat"
                              style={{display: `${showCategories ? "block" : "none"}`}}>
@@ -206,7 +206,7 @@ function SidebarAnnotation({
                             </div>
                         </div>
                     )}
-                    <div className="sidebar-annotation-cat">{annotation.category.toLowerCase()}</div>
+                    <div className="sidebar-annotation-cat" onClick={switchShowCategories}>{annotation.category.toLowerCase()}</div>
                     <div className="sidebar-annotation-header-info">
                         <div className="sidebar-annotation-header-info-user">{username}</div>
                     </div>
